b2ec266759b36265a478e83a0d99c3fec08df27c
yuhyuh <michael.mcgaw@pop.belmont.edu>
2019-11-21 16:39:11 -0600

    Skeleton

--- src/sample/Database_Access.java
+++ src/sample/Database_Access.java
@@ -1,9 +1,7 @@
 package sample;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.*;
+import java.util.Scanner;
 
 public class Database_Access extends Database_test{
 
@@ -131,5 +129,61 @@ public class Database_Access extends Database_test{
             System.out.println(e);
         }
         return infop;
+    }
+    //MichaelM Code
+    public static void addToDataBase(String name, String activity){
+        Connection conn = null;
+        Statement stmt = null;
+      //  Scanner scan = new Scanner(System.in);
+        try{
+            //STEP 2: Register JDBC driver
+            Class.forName("com.mysql.jdbc.Driver");
+
+            //STEP 3: Open a connection
+            System.out.println("Connecting to the selected database...");
+            conn = DriverManager.getConnection("jdbc:mysql://cleanearthsoftware.com:3306/4236paulgarlandclean_cims1",
+                    "cims_user1",
+                    "CSC4610-01");
+            System.out.println("Connected database successfully...");
+
+            stmt = conn.createStatement();
+            String sql = " INSERT INTO Customers(name, isActive)" +
+                    " VALUES (?,?)";
+
+
+            PreparedStatement preparedStatement = conn.prepareStatement(sql);
+//            System.out.println("What is the Customers ID?");
+//            String entry0 = scan.nextLine();
+          // preparedStatement.setString(1, entry0);
+          //  System.out.println("What is the customers name?");
+
+           // String entry1 = scan.nextLine();
+            preparedStatement.setString(1, name);
+       //     System.out.println("Is this customer active? 1 for yes, 0 for no");
+          //  String entry2 = scan.nextLine();
+            preparedStatement.setString(2, activity);
+            preparedStatement.executeUpdate();
+    //        System.out.println("You have successfully added " +  name + activity + "to the database!");
+
+        } catch(Exception se){
+            //Handle errors for JDBC
+            se.printStackTrace();
+        }//Handle errors for Class.forName
+        finally{
+            //finally block used to close resources
+            try{
+                if(stmt!=null)
+                    conn.close();
+            }catch(SQLException se){
+            }// do nothing
+            try{
+                if(conn!=null)
+                    conn.close();
+            }catch(SQLException se){
+                se.printStackTrace();
+            }//end finally try
+        }//end try
+        System.out.println("Goodbye!");
+
     }
 }
--- src/sample/Database_test.java
+++ src/sample/Database_test.java
@@ -1,9 +1,7 @@
 package sample;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.*;
+import java.util.Scanner;
 
 
 public class Database_test {
@@ -142,9 +140,112 @@ public class Database_test {
         }
         return true;
     }
+    // MichaelM; not quite sure where to put this shit
+    public static void addToDataBase() {
+        Connection conn = null;
+        Statement stmt = null;
+        Scanner scan = new Scanner(System.in);
+        try {
+
+            Class.forName("com.mysql.jdbc.Driver");
 
 
+            System.out.println("Connecting to the selected database...");
+            conn = DriverManager.getConnection(
+                    "jdbc:mysql://cleanearthsoftware.com:3306/4236paulgarlandclean_cims1",
+                    "cims_user1",
+                    "CSC4610-01");
+            System.out.println("Connected database successfully...");
 
+            stmt = conn.createStatement();
+            String sql = " INSERT INTO Customers(ID, name, isActive)" +
+                    " VALUES (?,?,?)";
+
+
+            PreparedStatement preparedStatement = conn.prepareStatement(sql);
+            System.out.println("What is the Customers ID?");
+            String entry0 = scan.nextLine();
+            preparedStatement.setString(1, entry0);
+            System.out.println("What is the customers name?");
+            String entry1 = scan.nextLine();
+            preparedStatement.setString(2, entry1);
+            System.out.println("Is this customer active? 1 for yes, 0 for no");
+            String entry2 = scan.nextLine();
+            preparedStatement.setString(3, entry2);
+            preparedStatement.executeUpdate();
+            System.out.println("You have successfully added " + entry0 + entry1 + entry2 + "to the database!");
+
+
+        } catch (SQLException se) {
+            //Handle errors for JDBC
+            se.printStackTrace();
+        } catch (Exception e) {
+            //Handle errors for Class.forName
+            e.printStackTrace();
+        } finally {
+            //finally block used to close resources
+            try {
+                if (stmt != null)
+                    conn.close();
+            } catch (SQLException se) {
+            }// do nothing
+            try {
+                if (conn != null)
+                    conn.close();
+            } catch (SQLException se) {
+                se.printStackTrace();
+            }//end finally try
+        }//end try
+
+    }
+    public static void removeFromDatabase(){
+        Connection conn = null;
+        Statement stmt = null;
+        Scanner scan = new Scanner(System.in);
+        try{
+
+            Class.forName("com.mysql.jdbc.Driver");
+
+
+            System.out.println("Connecting to the selected database...");
+            conn = DriverManager.getConnection(
+            "jdbc:mysql://cleanearthsoftware.com:3306/4236paulgarlandclean_cims1",
+                    "cims_user1",
+                    "CSC4610-01");
+            System.out.println("Connected database successfully...");
+
+            stmt = conn.createStatement();
+            String sql = " DELETE FROM Customers where name = ?";
+
+
+            PreparedStatement preparedStatement = conn.prepareStatement(sql);
+            System.out.println("What is the Customers name?");
+            String entry0 = scan.nextLine();
+            preparedStatement.setString(1, entry0);
+            preparedStatement.executeUpdate();
+            System.out.println("You have successfully removed " + entry0 +  "from the database!");
+
+        }catch(SQLException se){
+            //Handle errors for JDBC
+            se.printStackTrace();
+        }catch(Exception e){
+            //Handle errors for Class.forName
+            e.printStackTrace();
+        }finally{
+            //finally block used to close resources
+            try{
+                if(stmt!=null)
+                    conn.close();
+            }catch(SQLException se){
+            }// do nothing
+            try{
+                if(conn!=null)
+                    conn.close();
+            }catch(SQLException se){
+                se.printStackTrace();
+            }//end finally try
+        }//end try
+    }
 }
 
 
